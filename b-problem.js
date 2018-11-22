38f4cabeeb7eacd29e2b7a3f117c6483baa947ad
Junbin.lai <Junbin.lai@outlook.com>
2018-11-22 18:41:02 +0800

    modify

--- b-problem.js
+++ b-problem.js
@@ -1,1 +1,12 @@
-// document.getElementsByTagName('');
+window.onresize = function(){
+    var con = document.getElementsByClassName('content')[0];
+    if( window.innerWidth <= 574){
+       con.style= 'top:80px;';
+    }else if(window.innerWidth > 574){
+        con.style= 'top:60px;';
+    }
+}
+
+
+
+
