03323ab1d4eff9fcccaf5eb3f20eef466eba4472
Junbin.lai <Junbin.lai@outlook.com>
2018-11-29 15:13:35 +0800

    modify

--- b-problem.js
+++ b-problem.js
@@ -1,12 +1,13 @@
 window.onresize = function(){
     var con = document.getElementsByClassName('content')[0];
-    if( window.innerWidth <= 574){
-       con.style= 'top:80px;';
-    }else if(window.innerWidth > 574){
-        con.style= 'top:60px;';
+    w = window.innerWidth;
+    if( w<=294&&w>284){
+       con.style= 'Top:120px;';
+    }else if(w<=284&&w>168){
+        con.style= 'top:135px;';
+    }else if(w<=168&&w>158){
+        con.style= 'top:170px;';
+    }else{
+        console.log('ss');
     }
 }
-
-
-
-
